cc7cd15fff6f3f26bd199dc071ed1099fa5dc839
shreeharisvimal <ssvambalapuzha@gmail.com>
2024-08-31 13:21:57 +0530

    changes to manifest.json5

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
- import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
       <ToastContainer />
     </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-reportWebVitals();
+
 serviceWorkerRegistration.register();
+
+reportWebVitals();
