98fd11d2d467a7e0a3655821d9aecce298378196
shreeharisvimal <ssvambalapuzha@gmail.com>
2024-08-31 15:12:01 +0530

    added changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+ import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
       <ToastContainer />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
 );
-
-serviceWorkerRegistration.register();
-
 reportWebVitals();
